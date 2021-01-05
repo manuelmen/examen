b97790e975380002855fa77e14cb702dae7a6240
manuelmen <e0959334798@live.uleam.edu.ec>
2021-01-05 15:31:14 -0500

    primer commit

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAN0pQGsJqTV95wSvUfvFQOjMLz9QK_bvA",
+    authDomain: "biketours-f3d93.firebaseapp.com",
+    projectId: "biketours-f3d93",
+    storageBucket: "biketours-f3d93.appspot.com",
+    messagingSenderId: "742697361966",
+    appId: "1:742697361966:web:3b1050192666a00cf3b5bf",
+    measurementId: "G-RLTL755PRQ"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
